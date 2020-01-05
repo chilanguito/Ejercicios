18fa7520a545db66cd6b17d573e67dce0cc82bbc
Diego N <chilanguito96@outlook.es>
2020-01-05 01:52:52 -0600

    primer ejercicio

---
+++ src/practica/MyArrayInterface.java
@@ -0,0 +1,9 @@
+package practica;
+
+import java.util.ArrayList;
+
+interface MyArrayInterface {
+  void convertToDynamicArray(String[] a);
+  void clearIndex(int index);
+  ArrayList<String> obtainArray();
+}
