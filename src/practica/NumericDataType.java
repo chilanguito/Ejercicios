774f15c9c5b093c586be78b4a0347ffa50d88a5a
Diego N <chilanguito96@outlook.es>
2020-01-06 15:53:48 -0600

    Se tiene la salida correcta en consola

--- src/practica/NumericDataType.java
+++ src/practica/NumericDataType.java
@@ -4,26 +4,24 @@ public class NumericDataType implements Operations<Double> {
     @Override
     public void addition(Double a, Double b) {
         double result=a+b;
-        System.out.printf("%.2f",result);
-      //  System.out.printf("The addition of two numbers is:  "+"%.2d %n",result);
-        //System.out.printf("%+.3f %n", n);
+        System.out.printf("The addition of two numbers is: %.2f",result);
     }
 
     @Override
     public void subtraction(Double a, Double b) {
         double result=a-b;
-        System.out.println("The subtraction of two numbers is:"+result);
+        System.out.printf("\nThe subtraction of two numbers is: %.2f",result);
     }
 
     @Override
     public void multiplication(Double a, Double b) {
         double result=a*b;
-        System.out.println("The multiplication of two numbers is:"+result);
+        System.out.printf("\nThe multiplication of two numbers is: %.2f",result);
     }
 
     @Override
     public void division(Double a, Double b) {
         double result=a/b;
-        System.out.println("The division of two numbers is: "+result);
+        System.out.printf("\nThe division of two numbers is: %.2f",result);
     }
 }
